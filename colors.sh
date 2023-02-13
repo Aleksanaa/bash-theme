625be7ca21269f61bae20057c98c55941d1fc9a3
aleksana <me@aleksana.moe>
2023-02-14 05:36:28 +0800

    initial commit

---
+++ colors.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# These are rgb colors from nord palette.
+rgb_blue='81a1c1'
+rgb_green='a3be8c'
+rgb_yellow='ebcb8b'
+rgb_red='bf616a'
+rgb_orange='d08770'
+rgb_purple='b48ead'
+rgb_white='e5e9f0'
+
+
+# We can turn them into 
+
+_color() {
+	local rgb_name=rgb_$1
+	local rgb_color=${!rgb_name}
+	printf "\x1b[38;2;%d;%d;%dm" 0x${rgb_color:0:2} 0x${rgb_color:2:2} 0x${rgb_color:4:2}
+	printf "\033[1m"
+}
+
+_reset() {
+	printf "\033[0m"
+}
---
+++ icons.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+icon_nix=''
+icon_clock=''
+icon_home=''
+icon_arrow='❯'
+icon_git=''
+icon_warn=''
