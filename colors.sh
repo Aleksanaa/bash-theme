c58d306b1155fe0f5abce0da0b605ccfc2bb673f
aleksana <me@aleksana.moe>
2023-02-14 07:53:02 +0800

    fix prompt wrapping

--- colors.sh
+++ colors.sh
@@ -10,15 +10,17 @@ rgb_purple='b48ead'
 rgb_white='e5e9f0'
 
 
-# We can turn them into 
+# We can turn them into ANSI escapes
+# For wrapping prompt we need to add \[ and \]
+# See unix.stackexchange.com/questions/105958/
 
 _color() {
 	local rgb_name=rgb_$1
 	local rgb_color=${!rgb_name}
-	printf "\x1b[38;2;%d;%d;%dm" 0x${rgb_color:0:2} 0x${rgb_color:2:2} 0x${rgb_color:4:2}
-	printf "\033[1m"
+	printf "\[\x1b[38;2;%d;%d;%dm\]" 0x${rgb_color:0:2} 0x${rgb_color:2:2} 0x${rgb_color:4:2}
+	printf "\[\033[1m\]"
 }
 
 _reset() {
-	printf "\033[0m"
+	printf "\[\033[0m\]"
 }
