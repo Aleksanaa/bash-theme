5d2af1607f0e00fa921257126e749db2cf5dcf10
aleksana <me@aleksana.moe>
2023-02-19 22:56:11 +0800

    update git prompt but not working

--- functions.sh
+++ functions.sh
@@ -3,10 +3,28 @@
 source ./colors.sh
 source ./icons.sh
 
+# source gitstatus plugin
+if which gitstatusd > /dev/null 2>&1; then
+	GITSTATUS_PLUGIN_PATH=$(realpath "$(dirname $(which gitstatusd))/../share/gitstatus")
+	source ${GITSTATUS_PLUGIN_PATH}/gitstatus.plugin.sh
+	HAS_GITSTATUS=1
+fi
+
+# trim function, from dylanaraps
+# https://github.com/dylanaraps/pure-sh-bible#trim-all-white-space-from-string-and-truncate-spaces
+trim_all() {
+    set -f
+    set -- $*
+    printf '%s\n' "$*"
+    set +f
+}
+
 # These two functions deal with current directory.
 shrink_path() {
 	local path=$1
 	path="${path/"$HOME"/\~}"
+	# shrink paths to one letter each
+	# inspiration from powerlevel10k
 	if [ ${#path} -gt 32 ]; then
 		IFS='/' read -r -a path_array <<< "${path}"
 		for entry in "${path_array[@]::${#path_array[@]}-1}"; do
@@ -23,6 +41,7 @@ shrink_path() {
 }
 
 # Implements lazy loading on path
+# Produce path again only when it changes
 get_path() {
 	CURRENT_REAL_PATH=$(pwd)
 	if [ ! "${CACHED_REAL_PATH}" == "${CURRENT_REAL_PATH}" ]; then
@@ -33,17 +52,13 @@ get_path() {
 }
 
 get_hostname() {
-	[ -z ${HOSTNAME} ] && HOSTNAME=$(cat /proc/sys/kernel/hostname)
-	_color purple
-	printf "${HOSTNAME}"
-	_reset
-}
-
-get_nixshell() {
-	_color blue
 	if [[ ${PATH} == /nix/store/* ]]; then
-		printf "${ICON_NIX}"
+		_color blue
+		printf "${ICON_NIX} "
+	else
+		_color purple
 	fi
+	printf "\h"
 	_reset
 }
 
@@ -55,4 +70,21 @@ get_arrow() {
 	fi
 	printf "${ICON_ARROW}"
 	_reset
+}
+
+get_gitstatus() {
+	if [ -n ${HAS_GITSTATUS} ]; then
+		local output='';
+		if gitstatus_query && [[ "$VCS_STATUS_RESULT" == ok-sync ]]; then
+			if [[ -n "$VCS_STATUS_LOCAL_BRANCH" ]]; then
+				output+=" ${VCS_STATUS_LOCAL_BRANCH//\\/\\\\}"  # escape backslash
+			else
+				output+=" @${VCS_STATUS_COMMIT//\\/\\\\}"       # escape backslash
+			fi
+			(( VCS_STATUS_HAS_STAGED )) && output+='+'
+			(( VCS_STATUS_HAS_UNSTAGED )) && output+="${ICON_PENCIL}"
+			(( VCS_STATUS_HAS_UNTRACKED )) && output+='?'
+		fi
+		printf "${output}"
+	fi
 }
